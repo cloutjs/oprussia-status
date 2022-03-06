ddb5d506deafa37befa34910af1a5fef533c1749
clout <59515681+cloutjs@users.noreply.github.com>
2022-03-06 19:15:43 +0100

    Update and rename main (2).py to update-status.py

--- update-status.py
+++ update-status.py
@@ -2,20 +2,18 @@ import os
 import requests
 import json
 import time
-import keep_alive
 
-token=os.environ['apikey']
-gist_id="b6e7e2150e5e0a5804968aea3cf85986"
-filename="links.txt"
-
-keep_alive.keep_alive()
+token = "github api token"
+gist_id = "gist id"
+filename = "links.txt"
+github_user = "github username"
 
 while True:
     e = "by portia (https://twitter.com/portia1337)<br><br>"
     urls = open('list.txt', 'r')
     for url in urls:
       try:
-        r = requests.get(url.strip(), timeout=5)
+        r = requests.get(url.strip(), timeout=15)
         if r.status_code == 200:
             e = str(e) + f"{url.strip()} - Up<br>"
         elif r.status_code == 403:
@@ -27,6 +25,6 @@ while True:
     headers = {'Authorization': f'token {token}'}
     r = requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files':{filename:{"content":e}}}),headers=headers) 
     time.sleep(65)
-    r = requests.get("https://gist.githubusercontent.com/cloutjs/b6e7e2150e5e0a5804968aea3cf85986/")
+    r = requests.get("https://gist.githubusercontent.com/ + github_user + "/" + gist_id)
     time.sleep(200)
 
